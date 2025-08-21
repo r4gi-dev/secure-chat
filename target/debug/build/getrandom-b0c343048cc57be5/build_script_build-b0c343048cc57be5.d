adf7010941afd10295b51409f071b9e4f78dbc98
r4gi-dev <r4gi.github@gmail.com>
2025-08-21 21:28:42 +0900

    Remove file.txt

--- target/debug/build/getrandom-b0c343048cc57be5/build_script_build-b0c343048cc57be5.d
+++
@@ -1,5 +0,0 @@
-C:\secure-chat\target\debug\build\getrandom-b0c343048cc57be5\build_script_build-b0c343048cc57be5.d: C:\Users\metan\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\getrandom-0.3.3\build.rs
-
-C:\secure-chat\target\debug\build\getrandom-b0c343048cc57be5\build_script_build-b0c343048cc57be5.exe: C:\Users\metan\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\getrandom-0.3.3\build.rs
-
-C:\Users\metan\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\getrandom-0.3.3\build.rs:
--- target/debug/build/typenum-027f199633d186e3/build_script_build-027f199633d186e3.d
+++
@@ -1,5 +0,0 @@
-C:\secure-chat\target\debug\build\typenum-027f199633d186e3\build_script_build-027f199633d186e3.d: C:\Users\metan\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\typenum-1.18.0\build.rs
-
-C:\secure-chat\target\debug\build\typenum-027f199633d186e3\build_script_build-027f199633d186e3.exe: C:\Users\metan\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\typenum-1.18.0\build.rs
-
-C:\Users\metan\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\typenum-1.18.0\build.rs:
